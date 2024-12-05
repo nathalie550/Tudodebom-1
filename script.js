0a5e352cb906bda5bef6efd271a4d9cbdc5fd9bd
Nathalie-la-toile <nathalie.jean@latoile.em-lyon.com>
2024-12-05 17:24:08 +0100

    finalisation du site

---
+++ script.js
@@ -0,0 +1,115 @@
+
+  AOS.init();
+
+
+// Effectue une requête HTTP GET vers le fichier local "data.json" pour récupérer une liste de produits du restaurant
+fetch("../restaurant-bresilien.json")
+    .then((rep) => {
+        // Lorsque la réponse est reçue, elle est convertie en JSON pour être manipulée en tant qu'objet JavaScript
+        return rep.json();
+    })
+    .then((donnee) => {
+        // Une fois les données récupérées et converties, elles sont affichées dans la console pour vérification
+        console.log(donnee);
+        // Parcourt le tableau de donnee pour récupérer chaque plat 
+        donnee.plats.forEach((plat) => {
+            // Appelle la fonction 'afficherPlat' pour intégrer les cartes de chaque plat dans la page HTML
+            afficherPlat(plat);
+        });
+
+        donnee.services.forEach((service) => {
+            // Appelle la fonction 'afficher' pour intégrer les services dans la page HTML
+            afficherService(service);
+        });
+
+        donnee.temoignages.forEach((temoignage) => {
+            // Appelle la fonction 'afficher' pour intégrer les  temoignages dans la page HTML
+            afficherTemoignage(temoignage);
+        });
+    });
+
+
+// ** Fonction : afficher  les données du restaurant**
+// Rôle : Insérer dynamiquement les informations du restaurant dans la page HTML
+// Paramètre : "donnee" - un objet contenant les informations du restaurant
+// Retour : Aucun (la fonction manipule directement le DOM)
+function afficher(fichierJson) {
+    let nom = fichierJson.nomCommercial //nom du restaurant
+    let accroche = fichierJson.texteAccroche // le texte accrocheur
+    let textbtn = fichierJson.texteBouton // texte dans le btn "call action"
+
+    document.querySelector("heroContent").innerHTML += `
+                <h1>${nom}</h1>
+                <h2>${texteAccroche}</h2>
+                <a href="#" class="btn">${texteBouton}</a>
+    `
+}
+
+//CREATION DANS LA PARTIE HTML
+function afficherPlat(plat) {
+    let template =
+        `  <div class="card width40">
+                <img src= ${plat.imageurl}>
+                    <div class="cardDescription">
+                    <h4>${plat.nom}</h4>
+                    <p>${plat.desc}</p>
+                </div>
+            </div>`
+
+    // j'injecte chacune des petites carte dans ma page web dans la div qui a l'id divPlats
+    document.querySelector("#divPlats").innerHTML += template
+}
+
+
+function afficherService(service) {
+    let templateService =
+        `  <div class="carousselServiceCard" data-aos="zoom-out-left">              
+                        <h4>${service.nom}</h4>
+                <p>${service.desc}</p>
+                <a href="#" class="boutonService bold">"Reserver ICI"</a>
+            </div>`
+
+    // j'injecte chacune des petites carte dans ma page web dans la div qui a l'id divServices
+    document.querySelector("#divServices").innerHTML += templateService
+}
+
+
+function afficherTemoignage(temoignage) {
+    let templatetemoignage =
+        `  <div class="cardTemoignage" data-aos="fade-right">
+                <img src=assets/drapeau.jpg alt="photo d'un drapeau">
+                <div class="descriptionTemoignage">
+                    <h4>Type d'expérience : ${temoignage.typeExperience}</h4>
+                    <h5>Prénom : ${temoignage.prenom}</h5>
+                    <h6>note : ${temoignage.note}</h6>
+                    <p>${temoignage.commentaire}</p>
+                </div>
+            </div>`
+
+    // j'injecte chacune des petites carte dans ma page web dans la div qui a l'id divtemoignages
+    document.querySelector("#divtemoignages").innerHTML += templatetemoignage
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
