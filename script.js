0993447d41a7eee3e45d28d4a4f18b63953d724b
Nathalie-la-toile <nathalie.jean@latoile.em-lyon.com>
2024-12-06 20:06:36 +0100

    Site terminé

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-  AOS.init();
-
+AOS.init();
 
 // Effectue une requête HTTP GET vers le fichier local "data.json" pour récupérer une liste de produits du restaurant
 fetch("../restaurant-bresilien.json")
@@ -13,7 +12,7 @@ fetch("../restaurant-bresilien.json")
         // Une fois les données récupérées et converties, elles sont affichées dans la console pour vérification
         console.log(donnee);
         // Parcourt le tableau de donnee pour récupérer chaque plat     
-       
+
         donnee.plats.forEach((plat) => {
             // Appelle la fonction 'afficherPlat' pour intégrer les cartes de chaque plat dans la page HTML
             afficherPlat(plat);
@@ -27,30 +26,49 @@ fetch("../restaurant-bresilien.json")
         donnee.temoignages.forEach((temoignage) => {
             // Appelle la fonction 'afficher' pour intégrer les  temoignages dans la page HTML
             afficherTemoignage(temoignage);
-        });     
-              
+        });
+
+          // Appelle la fonction 'afficherFichierJson' pour insérer les données en haut du fichier dans la page HTML
+          afficherFichierJson(donnee);
+
+            // Appelle la fonction 'afficherPhrase' pour insérer la phrase commentaire dans la page HTML
+            afficherPhrase(donnee);
     });
 
 
-// ** Fonction : afficher  les données du restaurant**
-// Rôle : Insérer dynamiquement les informations du restaurant dans la page HTML
-// Paramètre : "donnee" - un objet contenant les informations du restaurant
-// Retour : Aucun (la fonction manipule directement le DOM)
-function  afficherFichierJson(fichierJson) {
-    let nom = fichierJson.nomCommercial //nom du restaurant
-    let accroche = fichierJson.texteAccroche // le texte accrocheur
-    let textbtn = fichierJson.texteBouton // texte dans le btn "call action"  
 
-    document.querySelector("#divCommentaires").innerHTML += divCommentaires
+/***********************FONCTION AFFICHER LES DONNEES DU RESTAURANT DANS LA PAGE HTML******************************/
+
+// Rôle : Insérer dynamiquement les informations du restaurant dans la page HTML
+// Paramètre : "donnee" objet contenant les informations du restaurant
+// Retour : Aucun (la fonction manipule directement le DOM)
+
+function afficherFichierJson(fichierJson) {
+    let divCommentaires = 
     `
-    <h1>${nom}</h1>
-    <h2>${accroche}</h2>
-    <a href="#" class="btn">${textbtn}</a>
-      `
+        <h1>${fichierJson.nomCommercial}</h1>
+        <h2>${fichierJson.texteAccroche}</h2>
+        <a href="#" class="btn">${fichierJson.texteBouton}</a>
+    `;
+   
+    document.querySelector("#divCommentaires").innerHTML = divCommentaires;
 }
 
 
-//CREATION DANS LA PARTIE HTML
+/************************FONCTION POUR AFFICHER LA PHRASE COMMENTAIRE DYNAMIQUEMENT DANS LE HTML*****************/
+function afficherPhrase(fichierJson) {
+    let divDivers = '';
+
+    // Boucle pour parcourir chaque promesse dans le tableau 'promessesClient'
+    fichierJson.promessesClient.forEach((promesse) => {
+        divDivers += `<p>${promesse}</p>`;
+    });
+  
+    document.querySelector(".divers").innerHTML = divDivers;
+}
+
+
+/****************************************CREATION DANS LA PARTIE HTML DES PLATS***********************************/
 function afficherPlat(plat) {
     let template =
         `  <div class="card width40">
@@ -66,10 +84,10 @@ function afficherPlat(plat) {
 }
 
 
-
+/*********************************CREATION DANS LA PARTIE HTML DES SERVICES**************************************/
 function afficherService(service) {
     let templateService =
-        `  <div class="carousselServiceCard" data-aos="zoom-out-left">              
+        `  <div class="carousselServiceCard" data-aos="flip-left">              
                         <h4>${service.nom}</h4>
                 <p>${service.desc}</p>
                 <a href="#" class="boutonService bold">"Reserver ICI"</a>
@@ -80,9 +98,10 @@ function afficherService(service) {
 }
 
 
+/*********************************CREATION DANS LA PARTIE HTML DES TEMOIGNAGES***********************************/
 function afficherTemoignage(temoignage) {
     let templatetemoignage =
-        `  <div class="cardTemoignage" data-aos="fade-right">
+        `  <div class="cardTemoignage" data-aos="flip-down"">
                 <img src=assets/drapeau.jpg alt="photo d'un drapeau">
                 <div class="descriptionTemoignage">
                     <h4>Type d'expérience : ${temoignage.typeExperience}</h4>
